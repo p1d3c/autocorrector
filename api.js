b60ee938d62e0e5c624e1f387372a7d792cece07
p1d3c <mrkomnot@yandex.ru>
2022-07-15 15:46:52 +0300

    v 1.0

---
+++ api.js
@@ -0,0 +1,40 @@
+const BASE_URL = 'http://speller.yandex.net/services/spellservice.json/checkText?text=';
+
+class Api {
+    constructor({ baseUrl, headers }) {
+        this.baseUrl = baseUrl;
+        this.headers = headers;
+    }
+
+    _getResponseData(res) {
+        if (!res.ok) {
+            return Promise.reject(`Ошибка ${res.status}`);
+        }
+        return res.json();
+    }
+
+    _convertText(text) {
+        return text.replace(' ', '+');
+    }
+
+    checkText(text) {
+        const convertedText = this._convertText(text);
+        return fetch(this.baseUrl + `${convertedText}`, {
+            method: 'POST',
+            headers: this.headers,
+            body: JSON.stringify({})
+        })
+            .then((res) => {
+                return this._getResponseData(res);
+            })
+    }
+}
+
+const api = new Api({
+    baseUrl: BASE_URL,
+    hedaers: {
+        'Content-Type': 'application/json; charset-UTF-8'
+    }
+})
+
+export default api;
---
+++ popup.js
@@ -0,0 +1,48 @@
+import api from './api.js';
+
+const form = document.querySelector('.form');
+const textArea = document.querySelector('.textarea');
+let changeTimer = false;
+const re = /[,.!?;:()]/;
+
+const apiRequest = (text, textArray) => {
+    api.checkText(text)
+    .then((data) => {
+        console.log(data)
+        console.log(textArray)
+        textArray.forEach((incorrect, i) => {
+            data.forEach((correct) => {
+                if (re.test(incorrect)) {
+                    incorrect = incorrect.slice(0, -1)
+                }
+                if (incorrect === correct.word) {
+                    textArray[i] = correct.s[0];
+                }
+            })
+        })
+        textArea.value = textArray.join(' ');
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const handleSubmit = (evt, textArea) => {
+    evt.preventDefault();
+    const textArray = textArea.value.split(' ');
+    apiRequest(textArea.value, textArray);
+}
+
+form.addEventListener('submit', (evt) => {
+    handleSubmit(evt, textArea);
+});
+
+form.addEventListener('keydown', (evt) => {
+    if(changeTimer) {
+        clearTimeout(changeTimer);
+    }
+    changeTimer = setTimeout(() => {
+        handleSubmit(evt, textArea);
+        changeTimer = false;
+    }, 750)
+})
